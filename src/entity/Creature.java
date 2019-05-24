f33f966370eaece737e01a90f586b1700bfbd908
dpq3005 <41153934+dpq3005@users.noreply.github.com>
2019-05-24 13:22:54 +0700

    update size, move

--- src/entity/Creature.java
+++ src/entity/Creature.java
@@ -1,13 +1,49 @@
 package entity;
 
 public abstract class Creature extends Entity {
+	public static final int DEFAULT_LIVE=2;
+	public static final int DEFAULT_WIDTH=20;
+	public static final int DEFAULT_HEIGHT=20;
+	public static final float DEFAULT_SPEED=2.0f;
 	protected int lives;
+	protected float speed;
+	protected float xMove,yMove;
 	
-	
-	public Creature(float x, float y) {
-		super(x, y);
-		lives = 2;
+
+	public Creature(float x, float y,int width,int height) {
+		super(x, y,width,height);
+		lives = DEFAULT_LIVE;
+		speed=DEFAULT_SPEED;
+		xMove=0;
+		yMove=0;
+		
 	}
+	public void move() {
+		x+=xMove;
+		y+=yMove;
+	}
+	
+	
+	public float getxMove() {
+		return xMove;
+	}
+
+
+	public void setxMove(float xMove) {
+		this.xMove = xMove;
+	}
+
+
+	public float getyMove() {
+		return yMove;
+	}
+
+
+	public void setyMove(float yMove) {
+		this.yMove = yMove;
+	}
+
+
 	
 	
 	
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -15,27 +15,38 @@ public class Player extends Creature {
 	private Game game;
 	private BufferedImage pacman;
 	public Player(Game game, float x, float y) {
-		super(x, y);
+		super(x, y,Creature.DEFAULT_WIDTH,Creature.DEFAULT_HEIGHT);
 		this.game = game;
 	}
 
 	@Override
 	public void tick() {
-		if(game.getKeyManager().up)
-			y -= 3;
-		if(game.getKeyManager().down)
-			y += 3;
-		if(game.getKeyManager().left)
-			x -= 3;
-		if(game.getKeyManager().right)
-			x += 3;
+		getInput();
+		move();
 		
 		
+	}
+	public void getInput() {
+		xMove=0;
+		yMove=0;
+		
+		if(game.getKeyManager().up) {
+			yMove= -speed;
+		}
+		if(game.getKeyManager().down) {
+			yMove= speed;
+		}
+		if(game.getKeyManager().right) {
+			xMove= speed;
+		}
+		if(game.getKeyManager().left) {
+			xMove= -speed;
+		}
 	}
 
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(Assets.pacman,(int) x, (int) y, null);
+		g.drawImage(Assets.pacman,(int) x, (int) y,width,height, null);
 	}
 	
 }
