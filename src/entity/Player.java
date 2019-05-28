231504adeec3658e746d90b1b48de13308fa62d3
ititiu14078 <32839172+ititiu14078@users.noreply.github.com>
2019-05-28 23:17:01 +0700

    Update Player.java

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -4,16 +4,16 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
-
 import Graphics.Assets;
 import game.Game;
-
 
 
 public class Player extends Creature {
 	
 	private Game game;
 	private BufferedImage pacman;
+	
+	
 	public Player(Game game, float x, float y) {
 		super(x, y,Creature.DEFAULT_WIDTH,Creature.DEFAULT_HEIGHT);
 		this.game = game;
@@ -48,5 +48,7 @@ public class Player extends Creature {
 	public void render(Graphics g) {
 		g.drawImage(Assets.pacman,(int) x, (int) y,width,height, null);
 	}
+	
+	
 	
 }
