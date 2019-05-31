612840ca12742e0674d88a501275e6def0b281fd
Ngoc99 <hodangphuongngoc191099@gmail.com>
2019-05-31 09:59:25 +0700

    entity Manager

--- src/World/World.java
+++ src/World/World.java
@@ -3,41 +3,80 @@ package World;
 
 
 import java.awt.Graphics;
-
-
+import game.HandleClass;
+import World.Utils;
+import entity.Creature;
+import entity.EntityManager;
+import entity.Player;
+import entitystatic.Coin;
 import tiles.Tile;
+
 
 
 public class World {
 	private int width,height;
+	private HandleClass handler; 
 	public static int [][] tiles;
 	private int spawnX, spawnY;
+	private EntityManager entityManager;
 	
-	public World(String path) {
-		loadWorld(path);
+
+	
+	
+	public World(HandleClass handler, String path) {
 		
+		
+		entityManager = new EntityManager(handler, new Player (handler,30,30));
+		
+		loadWorld(path);
+	
+		entityManager.getPlayer().setX((int)spawnX);
+		entityManager.getPlayer().setY((int)spawnY);
+		
+		entityManager.addEntity(new Coin (handler,30*2, 30));
+	
 		
 	}
+	
+	
+	
 	public void tick() {
+		entityManager.tick(); 
 		
 	}
 	public void render(Graphics g) {
 		for(int y=0;y<height;y++) {
 			for(int x=0;x<width;x++) {
 				getTile(x,y).render(g, x*Tile.TILEWIDTH, y*Tile.TILEHEIGHT);
+				//entityManager.render(g, x*Creature.DEFAULT_WIDTH, y*Creature.DEFAULT_HEIGHT);
+				//
 			}
+			
 		}
+		entityManager.render(g);
+		
 		
 	}
-	public Tile getTile(int x, int y) {
+	
+	
+	
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+	
+	
+	public  Tile getTile(int x, int y) {
 		if(x < 0 || y < 0 || x >= width || y >= height)
 			return Tile.blackTile;
 		
 		Tile t = Tile.tiles[tiles[x][y]];
+		
 		if(t==null)
-			return Tile.coin;
+			return Tile.blackTile;
 		return t;
 	}
+	
+	
 	private void loadWorld(String path) {
 		/*width=10;
 		height=10;
@@ -48,7 +87,9 @@ public class World {
 				tiles[x][y]=0;
 				
 			}*/
-
+		
+		
+		//load words from file world1.txt
 		String files =Utils. loadFileAsString(path);
 		String[] tokens = files.split("\\s+");
 		width = Utils. parseInt(tokens[0]);
@@ -56,19 +97,30 @@ public class World {
 		spawnX = Utils.parseInt(tokens[2]);
 		spawnY = Utils.parseInt(tokens[3]);
 
-		tiles= new int[width][height];
+		tiles= (new int[width][height]);
 		
 		for ( int y=0; y<height; y++){
 			for ( int x=0; x< width; x++){
 				tiles [x][y]= Utils.parseInt(tokens[(x+y*width)+4]);
 			}
-
-
 		}
-
-
-		
 	}
 	
-
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public HandleClass getHandler() {
+		return handler;
+	}
+	public void setHandler(HandleClass handler) {
+		this.handler = handler;
+	}
+	
+	
 }
---
+++ src/entity/EntityManager.java
@@ -0,0 +1,91 @@
+package entity;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import game.HandleClass;
+import entity.Player;
+import entitystatic.Coin; 
+
+public class EntityManager {
+	
+	private HandleClass handler;
+	
+	
+	private Player player;
+	
+	
+	
+	private ArrayList<Entity> entities;
+	
+
+	public EntityManager(HandleClass handler,Player player) {
+		this.handler= handler; 
+		this.player= player; 
+		entities= new ArrayList<Entity>();
+		addEntity(player);
+		
+		
+		
+		
+	}
+	
+	public void tick() {
+		Iterator<Entity> it = entities.iterator();
+		while(it.hasNext()){
+			Entity e = it.next();
+			e.tick();
+			
+		}
+		//player.tick();
+		
+	}
+	
+	
+	
+	public void render(Graphics g) {
+		for(Entity e : entities){
+			e.render(g);
+		}
+		//player.render(g);
+		
+	}
+	
+	
+	public void addEntity(Entity e ) {
+		
+		entities.add(e);
+		
+	}
+	
+	//GETTERS && SETTERS
+
+	public HandleClass getHandler() {
+		return handler;
+	}
+
+	public void setHandler(HandleClass handler) {
+		this.handler = handler;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	public ArrayList<Entity> getEntities() {
+		return entities;
+	}
+
+	public void setEntities(ArrayList<Entity> entities) {
+		this.entities = entities;
+	}
+	
+	
+	
+	
+}
